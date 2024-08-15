6dad50df0253cfe6216356003e6dbdd831358257
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-08-15 23:12:08 +0530

    practice java script

---
+++ day1/script.js
@@ -0,0 +1,12 @@
+let intro = "My name is  ASHAMIN BISWAS"
+
+console.log(typeof 20);
+
+
+const username = "ASHAMIN BISWAS"
+
+var age = 20;
+
+let userIntro = intro + " I am " + age + " " + "years old! yes" 
+
+console.log(userIntro);
